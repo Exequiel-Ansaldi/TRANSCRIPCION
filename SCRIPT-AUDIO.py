52405ff7ec81d23601115879a671cf2c7419dce0
Exequiel-Ansaldi <exeansaldi0@gmail.com>
2025-06-03 17:39:31 -0300

    Update SCRIPT-AUDIO.py

--- SCRIPT-AUDIO.py
+++ SCRIPT-AUDIO.py
@@ -1,12 +1,12 @@
 import os
 from pydub import AudioSegment
 import speech_recognition as sr
+# Asegurarse de que Pydub y SpeechRecognition están instalados
+# Puedes instalarlo con pip si no lo tienes:
 
 # Setear variables de entorno ANTES de usar Pydub
 os.environ["PATH"] += os.pathsep + r"C:\Users\GAMER\Downloads\ffmpeg-7.1.1\bin"
 
-from pydub import AudioSegment
-import speech_recognition as sr
 
 # También forzamos las rutas directamente por seguridad
 AudioSegment.converter = r"C:\Users\GAMER\Downloads\ffmpeg-7.1.1\bin\ffmpeg.exe"
@@ -15,7 +15,7 @@ AudioSegment.ffprobe = r"C:\Users\GAMER\Downloads\ffmpeg-7.1.1\bin\ffprobe.exe"
 print("✅ FFMPEG y FFPROBE están listos.")
 
 # Ruta del archivo MP3 original
-audio_mp3 = "C:/Users/GAMER/Pictures/TP-SO/10_a_20.mp3"
+audio_mp3 = "ARCHIVO ORIGINAL.mp3"
 # Convertir MP3 a WAV mono 16kHz
 audio = AudioSegment.from_mp3(audio_mp3)
 audio = audio.set_channels(1).set_frame_rate(16000)
@@ -48,6 +48,5 @@ with open("transcripcion.txt", "w", encoding="utf-8") as f:
     f.write(transcripcion_total)
 
 print("✅ Transcripción final guardada en 'transcripcion.txt'")
-
 
 
